d8337ad090656d0caecaa9e950173772686fbe4f
ZeptoBST <57198468+ZeptoBST@users.noreply.github.com>
2023-04-19 22:34:03 -0400

    Initial release

---
+++ Blast trainer/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Blast_trainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lives = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // lives
+            // 
+            this.lives.AutoSize = true;
+            this.lives.Checked = true;
+            this.lives.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.lives.Location = new System.Drawing.Point(52, 57);
+            this.lives.Name = "lives";
+            this.lives.Size = new System.Drawing.Size(110, 21);
+            this.lives.TabIndex = 0;
+            this.lives.Text = "Aspect Ratio";
+            this.lives.UseVisualStyleBackColor = true;
+            this.lives.CheckedChanged += new System.EventHandler(this.lives_CheckedChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 132);
+            this.Controls.Add(this.lives);
+            this.Name = "Form1";
+            this.Text = "Earthworm Jim 3D ZTrainer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox lives;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Blast trainer/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Memory;
+
+namespace Blast_trainer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Mem m = new Mem();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                int pID = m.GetProcIdFromName("EarthwormJim3D");   //  ZInfo: get proc id of game
+
+                bool openProc = false;  // ZInfo: Is process open?
+
+                if (pID > 0) openProc = m.OpenProcess(pID); // ZInfo: Try opening process if proc ID is greater than 1.
+
+                if (openProc)   // ZInfo: If process open, do code
+                {
+                    //Config file stuff
+                    string sAttr;
+                    sAttr = ConfigurationManager.AppSettings.Get("aRatio");                
+
+
+                    //The pointer bytes are read from bottom to top in cheat engine. I was doing top to bottom at first, which of course made it not work.
+                    if (lives.Checked) m.WriteMemory("base+000FBEE8,4,34,B1C", "float", sAttr);
+
+                }
+
+            }
+        }
+
+        private void lives_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
